88710eb5d607c85e51f8668f53757f8c4ce8e77f
ArnoldGoncharenko <arnoldg@zoomengine.com>
2014-02-08 12:32:13 -0500

    Inventory.cpp

---
+++ Inventory.cpp
@@ -0,0 +1,67 @@
+#include "Inventory.h"
+#include "Item.h"
+#include <istream>
+#include <iostream>
+#include <list>
+#include <string>
+#include <cctype>
+
+namespace GameCore
+{
+
+	Inventory::Inventory(short size)
+	{
+		item sword("Shitty sword", 100, "large", "Weapon", 10, 10);
+		item* swordPointer = &sword;
+		inventory.push_front(sword);
+		delete swordPointer;
+		maxSize = size;
+	}
+
+	Inventory::Inventory()
+	{
+		maxSize = 10;
+	}
+
+	void Inventory::addItem(item temp)
+	{
+		if (inventory.size() < maxSize)
+		{
+			inventory.push_front(temp);
+		}
+	}
+
+	void Inventory::removeItem(item temp)
+	{
+		if (inventory.size() > 0)
+		{
+			inventory.push_front(temp);
+		}
+	}
+
+	void Inventory::sortItems()
+	{
+		if (inventory.size() < 0)
+		{
+			inventory.sort([](item & a, item & b)
+			{
+				return a.getItemName() < b.getItemName();
+			});
+		}
+	}
+
+	void Inventory::displayItems()
+	{
+		cout << "mylist contains: ";
+		for (list<item>::iterator it = inventory.begin(); it != inventory.end(); ++it)
+		{
+			cout << *it << endl;
+		}
+	}
+
+	ostream& operator<<(ostream& os, item& c)
+	{
+		return os << c.getItemName();
+	}
+
+}
